be9cdc8bd3ff75f513a0a975791a5e3565be95bb
nibali <nikbali@mail.ru>
2019-05-02 23:19:15 +0300

    изменил метод createAircraft, теперь вместо json body + File метод принимает отдельные строковые параметры вместо json объекта

--- src/main/java/com/mai/pilot_assistent/controller/AircraftController.java
+++ src/main/java/com/mai/pilot_assistent/controller/AircraftController.java
@@ -2,6 +2,7 @@ package com.mai.pilot_assistent.controller;
 
 import com.mai.pilot_assistent.controller.dto.CreateAircraftRequest;
 import com.mai.pilot_assistent.controller.dto.base.ErrorResponse;
+import com.mai.pilot_assistent.model.Aircraft;
 import com.mai.pilot_assistent.service.AircraftService;
 import com.mai.pilot_assistent.util.converters.ConvertUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,30 +23,44 @@ public class AircraftController {
         this.aircraftService = aircraftService;
     }
 
+    //TODO лютый костыль, нужно поменять на адвекватный json-body, но пока нет времени
     @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<?> createAircraft(@RequestPart("image") MultipartFile file, @RequestPart("aircraft") CreateAircraftRequest request){
-       return aircraftService.createAircraft(file, ConvertUtils.fromCreateAircraftRequestToAircraft(request))
-                .fold(
-                        ResponseEntity::ok,
+    public ResponseEntity<?> createAircraft(@RequestPart(value = "image", required = false) MultipartFile file,
+                                            @RequestPart(value = "name", required = true) String name,
+                                            @RequestPart(value ="year", required = false) String year,
+                                            @RequestPart(value ="length", required = false) String length,
+                                            @RequestPart(value ="wingspan", required = false) String wingspan,
+                                            @RequestPart(value ="height", required = false) String height,
+                                            @RequestPart(value ="cruisingSpeed", required = false) String cruisingSpeed,
+                                            @RequestPart(value ="maxSpeed", required = false) String maxSpeed,
+                                            @RequestPart(value ="enginePower", required = false) String enginePower){
+        try {
+            Aircraft aircraft = Aircraft.builder()
+                    .name(name)
+                    .year(year != null ? Integer.parseInt(year) : null)
+                    .length(length != null ? Double.parseDouble(length) : null)
+                    .height(height != null ? Double.parseDouble(height) : null)
+                    .wingspan(wingspan != null ? Double.parseDouble(wingspan) : null)
+                    .cruisingSpeed(cruisingSpeed != null ? Double.parseDouble(cruisingSpeed) : null)
+                    .maxSpeed(maxSpeed != null ? Double.parseDouble(maxSpeed) : null)
+                    .enginePower(enginePower != null ? Double.parseDouble(enginePower) : null)
+                    .build();
+            return aircraftService.createAircraft(file, aircraft)
+                    .fold(
+                            ResponseEntity::ok,
 
-                        (fail) ->ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                                .body(ErrorResponse.builder()
-                                                                    .errorText(fail.getMessage())
-                                                                    .build())
-                );
-    }
+                            (fail) ->ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                    .body(ErrorResponse.builder()
+                                            .errorText(fail.getMessage())
+                                            .build())
+                    );
+        }catch (NumberFormatException ex){
+            return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                    .body(ErrorResponse.builder()
+                            .errorText("Некорректный тип данных в одном из параметров")
+                            .build());
+        }
 
-    @PostMapping(value = "/create_special", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<?> createAircraft(@RequestPart("aircraft") CreateAircraftRequest request){
-        return aircraftService.createAircraft(request.getImage(), ConvertUtils.fromCreateAircraftRequestToAircraft(request))
-                .fold(
-                        ResponseEntity::ok,
-
-                        (fail) ->ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                .body(ErrorResponse.builder()
-                                        .errorText(fail.getMessage())
-                                        .build())
-                );
     }
 
     @GetMapping(value = "/get_all", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
